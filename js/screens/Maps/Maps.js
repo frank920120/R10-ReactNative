421cc3b3d8e697734ca6a76bd11fc4b33b1067fd
Frank Fang <frankfang2014@hotmail.com>
2019-05-31 22:46:39 -0700

    add map

--- js/screens/Maps/Maps.js
+++ js/screens/Maps/Maps.js
@@ -4,21 +4,22 @@ import MapView, { Marker } from "react-native-maps";
 import { styles } from "./styles";
 const Maps = () => {
   return (
-    <MapView
-      style={styles.map}
-      region={{
-        latitude: 49.2635987,
-        longitude: -123.1407987,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      }}
-      showsUserLocation={true}
-    >
-      <Marker
-        coordinate={{ latitude: 49.2635987, longitude: -123.1407987 }}
-        image={require("../../assets/images/map_pin.png")}
-      />
-    </MapView>
+    <Text>123</Text>
+    // <MapView
+    //   style={styles.map}
+    //   region={{
+    //     latitude: 49.2635987,
+    //     longitude: -123.1407987,
+    //     latitudeDelta: 0.0922,
+    //     longitudeDelta: 0.0421
+    //   }}
+    //   showsUserLocation={true}
+    // >
+    //   <Marker
+    //     coordinate={{ latitude: 49.2635987, longitude: -123.1407987 }}
+    //     image={require("../../assets/images/map_pin.png")}
+    //   />
+    // </MapView>
   );
 };
 
